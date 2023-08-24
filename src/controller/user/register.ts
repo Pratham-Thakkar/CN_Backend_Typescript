d165aca94f79432426566424ff3c2af3f6102064
Pratham-Thakkar <pratham.thakkar@talentsystems.com>
2023-08-24 15:12:27 +0530

    Model Created

---
+++ src/controller/user/register.ts
@@ -0,0 +1,9 @@
+import User from "../../model/user";
+import { Request, Response } from "express";
+
+// export const registerUser: Function = async (
+//   req: Request,
+//   res: Response
+// ): Promise<Response> => {
+
+// };
